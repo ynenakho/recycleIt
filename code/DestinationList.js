cd2a4538c01e340312928b78ba5b9d20497cbf1e
Yuriy Nenakhov <ynenakho@e1z4r15p7.42.us.org>
2019-04-15 20:56:37 -0700

    API calls to get info about recycling destinations

--- code/DestinationList.js
+++ code/DestinationList.js
@@ -1,6 +1,46 @@
-module.exports.function = function destinationList (response) {
-  
-  return {
-    name: "Gavno"
+var console = require('console');
+var http = require('http');
+var config = require('config');
+var secret = require('secret');
+var value = secret.get('earthApiKey');
+
+function GetListDestinations (locationId) {
+  var queryObject = {
+    api_key: value,
+    location_id: locationId
+  };
+  var queryString = http.makeQueryString(queryObject);
+  var response = http.getUrl(config.get('remote.earth.url') + "getLocationDetails?" + queryString, { passAsJson: true });
+  console.debug("GetListDestinations response = ",response);
+  var data = JSON.parse(response)
+  console.log("DATA =", data);
+
+  var resp = data.result[locationId];
+  var result = Object.assign({}, {
+    name: resp.description ? resp.description : "unknown",
+    address: resp.address ? resp.address : "unknown",
+    city: resp.city ? resp.city : "unknown",
+    state: resp.province ? resp.province : "unknown",
+    zip: resp.postal_code ? resp.postal_code : "unknown",
+    phone: resp.phone ? resp.phone : "unknown",
+    website: resp.url ? resp.url : "unknown",
+    latitude: resp.latitude ? resp.latitude : "unknown",
+    longitude: resp.longitude ? resp.longitude : "unknown",
+    materials: resp.materials ? resp.materials.map(material => material.description) : "unknown"
+  });
+
+  console.debug("GetListDestinations data =",result);
+  return result;
+}
+
+module.exports.function = function DestinationList (response) {
+  console.debug("DestinationList =", response.destinationPoint[0]);
+  let result = [];
+
+  for (let i = 0; i < response.destinationPoint.length; i++) {
+    result.push(GetListDestinations(response.destinationPoint[i].locationId));
   }
+
+  console.debug("RESULT!!! =", result)
+  return result;
 }
